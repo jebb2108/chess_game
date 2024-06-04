8db3181c00874bfdd83a6df1a6e28b2992bc7b1b
jebb king <gabouchard2002@gmail.com>
2024-06-04 18:19:27 +0300

    Chess Game ver.1.0 Final commit

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ class GamePlay(Board):
                 print('\nThe session is over')
                 break
 
+            # Команда разработчика.
             elif from_where_input == 'h+':
                 # gen0 = [(move[0].y, move[0].x) for move in self.all_moves.values()]
                 gen1 = [move[1] for move in self.all_moves.values()]
@@ -66,15 +67,17 @@ class GamePlay(Board):
                 for item in zip(gen1, gen2):
                     print(item)
 
-            from_where_input = from_where_input[0].title() + from_where_input[1]
-
             to_where_input = input('{}'.format(self.second_beginning_message))
             if to_where_input[0].lower() == 'q' or len(to_where_input) < 2:
                 print('\nThe session is over')
                 break
 
+            from_where_input = from_where_input[0].title() + from_where_input[1]
             to_where_input = to_where_input[0].title() + to_where_input[1]
 
+
+            # Переводит в числовые координаты 'E2' --> [6, 4]
+            # Если координаты некорректные, выдает ошибку.
             try:
                 from_where = self.settings.transcripts[from_where_input]
                 to_where = self.settings.transcripts[to_where_input]
@@ -83,6 +86,8 @@ class GamePlay(Board):
                 self.make_msg('E: You mistyped. Please try again')
                 self.print_board()
 
+            # Маленькая проверка на то, чтобы это была фигуры.
+            # Передает в следующую функцию.
             else:
                 res = self.get_class(from_where)
                 self.move_piece(res, from_where, to_where)
@@ -154,8 +159,8 @@ class GamePlay(Board):
                     self.force_change(obj, to_where, from_where)
                     return False
 
-            kings.object_copies.clear()
-            return self.post_check_king(obj, from_where, to_where)
+
+            return self.post_check_king(obj, from_where, to_where)  # noqa
 
 
         # То же самое для белого короля.
@@ -194,38 +199,63 @@ class GamePlay(Board):
 
                     return False
 
-            kings.object_copies.clear()
-            return self.post_check_king(obj, from_where, to_where)
+
+            return self.post_check_king(obj, from_where, to_where)  # noqa
 
 
 
-        # Сохраняю возвращенный элемент в переменную класса King.
+        # Если шаха не зафиксировано,
+        # программа пытается переместить фигуру на доске.
         temp_res = self.change_its_position(obj, to_where)
 
+        # Если результат отрицательный,
+        # происходит выброс из цикла действий.
         if temp_res is False: return False
-        if temp_res not in [True, None]:
+        if temp_res not in [True, None]:  # Иначе проверяет, есть ли другое значение от булевых?
             removed_piece_id = temp_res[0]
             removed_piece = temp_res[1]
+            # Перемещает экземпляр во временное хранилище - список.
             kings.object_copies.extend([removed_piece])
 
-            if self.post_check_king(obj, from_where, to_where):
-                self.all_moves[removed_piece_id] = (removed_piece, [])
+            # Так как изменения на доске уже произошли,
+            # проверяет, есть ли шах королю сейчас?
+            if self.post_check_king(obj, from_where, to_where):  # noqa
+
+                # Проверка выполнена.
+                # Фигура остается в той же позиции
+                # и съеденная вражеская фигура удаляется.
                 return True
+
             else:
+                # Король остается под шахом.
+                # Возвращает фигуру на место.
+                # Удаленная фигура возвращается
+                # в хранилище из резервного списка.
                 self.all_moves[removed_piece_id] = (removed_piece, [])
                 self.force_change(obj, to_where, from_where, kings.object_copies[-1])
                 kings.object_copies.clear()
+                # Проверка не пройдена. Возвращает False.
                 return False
+
+        # Это поле служит на случай,
+        # если res вернулось булево значение:
+        # True или (редко) None
         else:
 
-            if self.post_check_king(obj, from_where, to_where):
+            # Два возможных исхода после последней проверки.
+            # Помните, что передвижение уже выполнено.
+            if self.post_check_king(obj, from_where, to_where):  # noqa
                 return True
+
             else:
+                # Обратите внимание, что программа
+                # не возвращает ничего в аргумент removed_piece.
                 self.force_change(obj, to_where, from_where)
                 return False
 
 
     def post_check_king(self, obj, from_where, to_where):
+        """ Проверка на шах уже после сделанного хода. """
 
         if self.is_under_attack('black') and obj.color == 2:
 
@@ -255,27 +285,35 @@ class GamePlay(Board):
             # Останавливает.
             return False
 
+        kings.object_copies.clear()
         return True
 
         # Позволяет пройти проверку.
 
-    def is_under_attack(self, color):
+    def is_under_attack(self, color: str) -> [True or False]:
+        """ Проверка, если король находится в зоне атаки вражеской фигуры. """
 
         if color == 'black':
 
             return (self.all_moves[kings.black_king][0].safe_zone
-                    in set(sum([value[1] if value[0].color == 1 else value[1] for value in self.all_moves.values()], [])))
+                    in set(sum([value[1] if value[0].color == 1
+                                else value[1] for value in self.all_moves.values()], [])))
 
         elif color == 'white':
 
             return (self.all_moves[kings.white_king][0].safe_zone
-                    in set(sum([value[1] if value[0].color == 2 else value[1] for value in self.all_moves.values()], [])))
+                    in set(sum([value[1] if value[0].color == 2
+                                else value[1] for value in self.all_moves.values()], [])))
+
 
 
     def make_msg(self, e):
+        """ Подставляет сообщение об ошибке. """
         self.message = e
 
+
     def auto_print(self):
+        """Вспомогательная функция для печати доски и коррекции сообщений."""
 
         self.message = 'Press "q" to quit'
 
