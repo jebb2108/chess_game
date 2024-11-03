51b8e951db43285b4c69dec74d5737a8be80398c
Gabriel Bouchard <jebb@Gabriels-MacBook-Air.local>
2024-11-03 20:24:14 +0300

    added grid & made login button interactive

--- interface/game.py
+++ interface/game.py
@@ -29,13 +29,12 @@ class Game:
         self.board_rect = self.board.get_rect()
         self.board_rect.top, self.board_rect.left = 20, 20
 
-
+        self.board_dict = dict()
 
 
 
 
     def alter_time(self):
-
         current_time = self.default_time
         for index in range(0, len(Game.TIMES_LIST)):
             if current_time == Game.TIMES_LIST[index]:
@@ -55,16 +54,35 @@ class Game:
         else:
             self.choose_time_display.draw()
 
+    def show_borders(self, flag):
+        alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        offset_xy = 10
+        if flag:
+            rect = pygame.draw.rect(self.window, RED, (50, 50, 540, 540), 5)
+            left_border, top_border = rect[0] + 15, rect[1] + 15
+            height_adjustment = 0
+
+            for level in range(1, 9):
+                width_adjustment = 0
+
+                for num, char in enumerate(alpha, 0):
+                    pygame.draw.rect(self.window, GREEN, (left_border + width_adjustment, top_border + height_adjustment, 55, 55), 2)
+                    width_adjustment += 55 + offset_xy
+                    resulted_numeric_coords = [(left_border+width_adjustment, top_border+height_adjustment), (level-1, num)]
+                    visual_coords_by_alphabet = f'{str(char).title()}{str(level)}'
+                    self.board_dict[visual_coords_by_alphabet] = resulted_numeric_coords
+
+                height_adjustment += 55 + offset_xy
 
 
+        return None
 
 
-
-
-
-    def draw(self):
+    def draw(self, flag=False):
         self.window.fill(LIGHT_GRAY)
         for button in self.buttons:
             button.draw()
         self.check_time_button()
         self.window.blit(self.board, self.board_rect)
+        self.show_borders(flag)
+
--- interface/main_chess_game.py
+++ interface/main_chess_game.py
@@ -29,33 +29,48 @@ BOARD_IMAGE = pygame.image.load('images/chess_board.jpg')
 # 5 - Инициализируем переменные
 o_auth = Authorization(window)
 o_game = Game(window)
-login_awaiting_status = True
+login_awaiting_status = False
+flag = False
 
 
 # 6 - Бесконечный цикл
 while True:
+    board_dict = None
     # 7 - Проверяем на наличие событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             sys.exit()
 
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_m:
+                print(board_dict)
+                flag = not flag
+
+
+
         if login_awaiting_status:
             o_auth.login_input.handleEvent(event)
             o_auth.password_input.handleEvent(event)
+
+            if o_auth.login_input.getValue() != '':
+                if o_auth.password_input.getValue() != '':
+                    o_auth.login_button.enable()
+            else:
+                o_auth.login_button.disable()
+
             o_auth.draw()
 
             if o_auth.login_button.handleEvent(event) and o_auth.check_authorization():
                 login_awaiting_status = False
                 break
-
-
         else:
             o_game.new_game_button.handleEvent(event)
             if o_game.choose_time_button.handleEvent(event):
                 o_game.alter_time()
 
-            o_game.draw()
+            board_dict = o_game.draw(flag)
+
 
             if o_game.quit_button.handleEvent(event):
                 o_auth.login_input.setValue('')
